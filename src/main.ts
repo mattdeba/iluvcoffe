bc7c95b179f74b3e832a353acb2c0238fc3d3caf
matthieu debarge <debarge.matthieu@gmail.com>
2023-03-30 20:41:56 +0200

    dto validation avoid suplementary fields

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //permet de filtrer les body des requêtes en ne prenant que les valeurs définies dans le dto.
+      forbidNonWhitelisted: true, // n'autorise pas les champs non définis
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
