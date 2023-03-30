9c84fd4de296876fda1be7790f769ce1346ac2c6
matthieu debarge <debarge.matthieu@gmail.com>
2023-03-30 21:24:00 +0200

    setup db

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffe.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffesController],
   providers: [CoffesService],
 })
