799816bf831e1d025bde1128e27123944bd52d58
matthieu debarge <debarge.matthieu@gmail.com>
2023-03-30 21:12:49 +0200

    connect to postgresql db running on docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+/* AppModule - FINAL CODE */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffesModule } from './coffes/coffes.module';
 
 @Module({
-  imports: [CoffesModule],
+  imports: [
+    CoffesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5432, // database host
+      username: 'postgres', // username
+      password: 'pass123', // user password
+      database: 'postgres', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
