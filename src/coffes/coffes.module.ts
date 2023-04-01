f26cfbf7fe42b4c96b7fdd6e6039c2fc1c876074
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-01 12:02:36 +0200

    modifier la valeur du provider avec un mock

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
 
+class MockCoffeeService {}
+
 @Module({
   controllers: [CoffesController],
-  providers: [CoffesService],
+  providers: [
+    {
+      provide: CoffesService,
+      useValue: new MockCoffeeService(),
+    },
+  ],
   exports: [CoffesService],
 })
 export class CoffesModule {}
