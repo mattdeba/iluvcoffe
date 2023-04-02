78813da11322f6fd68260978a451be35f6e27a4a
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-02 11:12:16 +0200

    modules dynamiques

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffesModule } from '../coffes/coffes.module';
 import { CoffesService } from '../coffes/coffes.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [CoffesModule],
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+    }), // Importation du module dynamique en lui passant les paramètres de connexion
+    CoffesModule,
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-@Module({
-  providers: [
-    {
-      provide: 'CONNECTION',
-      useValue: new DataSource({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-      }).initialize(),
-    },
-  ],
-}) //Implementation statique
+@Module({})
 export class DatabaseModule {
   static register(options: DataSourceOptions): DynamicModule {
     return {
