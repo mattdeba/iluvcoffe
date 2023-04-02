bc6e65a1db89c6be6d834446f1da776a588c516e
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-02 10:36:56 +0200

    factory providers

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -1,21 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
+import { COFFEE_BRANDS } from './coffees.constants';
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+@Injectable()
+class myProvider {
+  constructor(public myString: string = 'hello') {}
+}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['buddy brew', 'nescafe'];
+  }
+}
 
 @Module({
   controllers: [CoffesController],
   providers: [
     CoffesService,
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) => {
+        brandsFactory.create();
+      },
+      inject: [CoffeeBrandsFactory],
     },
   ],
   exports: [CoffesService],
