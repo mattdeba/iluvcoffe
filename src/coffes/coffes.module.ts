84b76b0a33a851ca14d5bbf02741a5c06e51cf00
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-02 10:22:19 +0200

    custom class providers

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   controllers: [CoffesController],
   providers: [
     CoffesService,
     {
-      provide: 'COFFEE_BRANDS',
-      useValue: ['buddy brew', 'nescafe'],
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
     },
   ],
   exports: [CoffesService],
